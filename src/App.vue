b2e48fc9403811e206b6551201d16cb5c75fcee5
Boldyreva Nadezhda <boldyrevanadezhda@gmail.com>
2023-04-11 00:34:18 +0700

    TASK-4. Анимация фона меню

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,24 @@
 </template>
 
 <style lang="scss">
+html {
+  height: 100vh;
+}
+
+body {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
+  width: 100%;
+  height: 100%;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,3 +16,55 @@ import { useRouter } from 'vue-router'
 
 const router = useRouter()
 </script>
+
+<style lang="scss">
+.home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  position: relative;
+
+  animation: scroll 70s linear infinite;
+  background: url(~@/assets/background-menu.jpg);
+  background-position: 3000px;
+  perspective: 1000px;
+  perspective-origin: 50% 50%;
+}
+
+.home::before {
+  content: '';
+
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+
+  animation: gradient 15s ease infinite;
+  background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+  background-size:  400% 400%;
+  
+  mix-blend-mode: multiply;
+}
+
+@keyframes scroll {
+  100% {
+    background-position: 0px -3000px;
+  }
+}
+
+@keyframes gradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+</style>
